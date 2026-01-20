fff106fc5a20a23a7b477fe4c469e045f43db07b
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2026-01-20 15:21:38 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// ---------- 1. BASICS ----------
+pub fn greet(name: &str) -> String {
+    // TODO: return "Hello, <name>!"
+    todo!()
+}
+pub fn sum(nums: &[i32]) -> i32 {
+    // TODO: sum all elements using an iterator
+    todo!()
+}
+pub fn flip(b: bool) -> bool {
+    // TODO: return the opposite boolean
+    todo!()
+}
