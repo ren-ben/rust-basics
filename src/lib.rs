beea0dd5a13ccc37ed8f9cf7b7043445a94b22a1
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2026-01-20 15:27:55 +0100

    add ownership & borrowing logic

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,16 @@ pub fn sum(nums: &[i32]) -> i32 {
 pub fn flip(b: bool) -> bool {
     !b
 }
+
+// ownership & borrowing
+pub fn take_ownership(s: String) -> usize {
+    s.len()
+}
+
+pub fn borrow_first_char(s: &str) -> Option<char> {
+    s.chars().next()
+}
+
+pub fn push_exclamation(s: &mut String) {
+    s.push('!');
+}
